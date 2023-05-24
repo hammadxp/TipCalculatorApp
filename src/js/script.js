754071dfbb367e40aa7088dee24e49b20cbb4a5b
hammadxp <hammadjaved9860@gmail.com>
2023-05-24 13:43:17 +0500

    design is complete, took 2 hours excluding html structure, nice

---
+++ src/js/script.js
@@ -0,0 +1,1 @@
+// Tip Amount / person Total / person Reset
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ["*.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        SpaceMono: ["Space Mono", "sans-serif"],
+      },
+      colors: {
+        "is-strong-cyan": "hsl(172, 67%, 45%)",
+        "is-very-dark-cyan": "hsl(183, 100%, 15%)",
+        "is-dark-grayish-cyan": "hsl(186, 14%, 43%)",
+        "is-grayish-cyan": "hsl(184, 14%, 56%)",
+        "is-light-grayish-cyan": "hsl(185, 41%, 84%)",
+        "is-very-light-grayish-cyan": "hsl(189, 41%, 97%)",
+        "is-white": "hsl(0, 0%, 100%)",
+      },
+      screens: {
+        "375px": { max: "375px" },
+      },
+    },
   },
   plugins: [],
-}
-
+};
